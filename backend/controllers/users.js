02fd6fd49e3d19a1fdf7c5c9b22505c54694bcf6
DariaP9315 <79569262+DariaP9315@users.noreply.github.com>
2021-10-06 20:52:42 +0400

    Update users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -18,8 +18,6 @@ module.exports.login = (req, res, next) => {
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: true,
           secure: true,
         })
         .send({ token });

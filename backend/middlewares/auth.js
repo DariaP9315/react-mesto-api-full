bc182df5a25bc1dbf138430b26c2b028ea915b27
DariaP9315 <79569262+DariaP9315@users.noreply.github.com>
2021-10-08 09:17:16 +0400

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,11 +5,14 @@ const UnauthorizedError = require('../errors/unauthorized-err'); // 401
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { jwt } = req.cookies;
+  if (!jwt) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jsonwebtoken.verify(jwt, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
   }

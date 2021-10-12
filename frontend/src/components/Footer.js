090c305c50ee0cf1279c569d96922cdb478c0ded
DariaP9315 <79569262+DariaP9315@users.noreply.github.com>
2021-10-12 13:28:22 +0400

    Delete frontend/src/components directory

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <div className='footer'>
-            <p className='footer__copyright'>&#169; 2021 Mesto Russia</p>
-        </div>
-    )
-}
-
-export default Footer
